de3301bde6b944ef0a837b2c6699e6c81dbf5f92
Shafquet Naghmi <naghmishafquet@gmail.com>
2023-05-01 10:02:30 +0530

    m

--- myshop/settings.py
+++ myshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=config('SECRET_KEY')
 DEBUG = False
 #DEBUG = config('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-0e528.up.railway.app/','http://127.0.0.1:8000/']
 
 
 # Application definition
