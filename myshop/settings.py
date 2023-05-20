28b025a3d64a2786fea6fd2693e71074a0804131
Shafquet Naghmi <naghmishafquet@gmail.com>
2023-05-20 12:18:52 +0530

    m

--- myshop/settings.py
+++ myshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 #DEBUG = config('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['web-production-0e528.up.railway.app','127.0.0.1:8000']
